b0c0323a886f747bb82583dc79be0e31df622555
Jake <90354992+jakezubov@users.noreply.github.com>
2024-04-16 06:45:38 +1000

    0.06 - Rebuilt Project
    - Had issue with project not opening so created a new project and copied code across.

--- frontier.client/src/Jewellery/RollingWire.jsx
+++ frontier.client/src/Jewellery/RollingWire.jsx
@@ -9,25 +9,32 @@ const RollingWire = () => {
     const [profile, setProfile] = useState('');
     const [width, setWidth] = useState('');
     const [thickness, setThickness] = useState('');
+    const [length, setLength] = useState('');
     const [stockSize, setStockSize] = useState('');
 
     // Calculated
+    const [lengthRingSizeSwitch, setLengthRingSizeSwitch] = useState(true);
     const [stockSizeRequired, setStockSizeRequired] = useState(true);
     const [output, setOutput] = useState('');
 
     const handleCalculate = () => {
-        const isDropdownsValid = ringSize !== undefined && profile !== ""
+        const isDropdownsValid = profile !== ""
         const isNumbersValid = validateNumber(width) && validateNumber(thickness) && (!stockSizeRequired || validateNumber(stockSize));
+        const isSwitchValuesValid = ringSize !== undefined || validateNumber(length)
 
-        if (isDropdownsValid && isNumbersValid) {
+        if (isDropdownsValid && isNumbersValid && isSwitchValuesValid) {
             setOutput("Valid Input");
         } else {
             setOutput("Invalid Input");
         }
     }
 
-    const handleCheckbox = () => {
-        !stockSizeRequired ? setStockSizeRequired(true)  : setStockSizeRequired(false), setStockSize('')
+    const handleStockCheckbox = () => {
+        !stockSizeRequired ? setStockSizeRequired(true) : setStockSizeRequired(false), setStockSize('')
+    }
+
+    const handleLengthRingSizeSwitch = () => {
+        !lengthRingSizeSwitch ? setLengthRingSizeSwitch(true) : setLengthRingSizeSwitch(false)
     }
 
     const handleRingSizeChange = (ringSize) => {
@@ -43,23 +50,43 @@ const RollingWire = () => {
             <h1>Rolling Wire</h1>
             <div className="container">
                 <div className="column">
-                    <div className="text">Ring Size</div>
                     <div className="text">Profile</div>
+                    <div>
+                        {
+                            lengthRingSizeSwitch ?
+                                <div className="text">Length <button type="button" onClick={handleLengthRingSizeSwitch}></button></div> :
+                                <div className="text">Ring Size <button type="button" onClick={handleLengthRingSizeSwitch}></button></div>
+                        }
+                    </div>
                     <div className="text">Width</div>
                     <div className="text">Thickness</div>
                     <div className="text">Stock Size Required</div>
                     <div>
-                        {stockSizeRequired ? <div className="text">Stock Size</div> : <div></div>}
+                        {
+                            stockSizeRequired ?
+                                <div className="text">Stock Size</div> :
+                                <div></div>
+                        }
                     </div>
                 </div>
                 <div className="column">
-                    <div><RingSizeSelector label="Ring Size" onSizeChange={handleRingSizeChange} /></div>
                     <div><ProfileSelector label="Profile" onProfileChange={handleProfileChange} /></div>
+                    <div>
+                        {
+                            lengthRingSizeSwitch ?
+                                <input type="number" step="0.01" value={length} onChange={(e) => setLength(e.target.value)} /> :
+                                <div><RingSizeSelector label="Ring Size" onSizeChange={handleRingSizeChange} /></div>
+                        }
+                    </div>
                     <input type="number" step="0.01" value={width} onChange={(e) => setWidth(e.target.value)} />
                     <input type="number" step="0.01" value={thickness} onChange={(e) => setThickness(e.target.value)} />
-                    <input type="checkbox" onClick={handleCheckbox} defaultChecked={true} />
+                    <input type="checkbox" onClick={handleStockCheckbox} defaultChecked={true} />
                     <div>
-                        {stockSizeRequired ? <input type="number" step="0.01" value={stockSize} onChange={(e) => setStockSize(e.target.value)} /> : <div></div>}
+                        {
+                            stockSizeRequired ?
+                                <input type="number" step="0.01" value={stockSize} onChange={(e) => setStockSize(e.target.value)} /> :
+                                <div></div>
+                        }
                     </div>
                 </div>
             </div>
